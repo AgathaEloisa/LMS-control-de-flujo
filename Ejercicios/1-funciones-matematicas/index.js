18eb27e4b0b559bc1fe4d352d1571f74e038ccc0
AgathaP <ebrumaktub@gmx.com>
2019-09-17 10:22:14 -0300

    3 de 4 ejercicios terminados. 1 de 2 casos practicos terminados

---
+++ Ejercicios/1-funciones-matematicas/index.js
@@ -0,0 +1,11 @@
+
+const add = (num1, num2) => {
+    return num1 + num2;
+};
+
+const multiply = (num1, num2) => {
+    return num1 * num2;
+}
+
+console.log(multiply(36325,add(9824,777)));
+// console.log(multiply(process.argv[2],add(9824,777)));
