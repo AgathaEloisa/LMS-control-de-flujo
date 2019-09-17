ff90810eb6adb2ffaf29b664f1e3ad1d0e0556f6
AgathaP <ebrumaktub@gmx.com>
2019-09-17 11:51:51 -0300

    Caso practico 2, terminado.

--- Casos-practicos/tablero-de-ajedrez/index.js
+++ Casos-practicos/tablero-de-ajedrez/index.js
@@ -1,9 +1,23 @@
-const chessboard = (str, size) => {
-    str = '#';
-    let board = str + ' ';
-    let repeat = board.repeat(8);
+let size = parseInt(prompt('tamaño de cuadricula'));
+let str = prompt('ingresa un caracter');
 
-   return console.log(repeat);
+const chessboard = (str, size) => {
+    // let board = str + ' ';
+    // let repeat = board.repeat(size);
+    // let final;
+    let result = '';
+
+    for(let i = 1 ;i <= size; i++){
+        for(let n = 1; n <= size; n++){
+            if((n + i) % 2 === 0){
+                result += ' ';
+            } else {
+                result += str;
+            }
+        }
+        result += '\n';
+    }
+   return result;
 }
 
-chessboard();
+console.log(chessboard(str, size));
