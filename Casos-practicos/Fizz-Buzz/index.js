83a3a0789e151e7f65aab5ec7d23063c687a07e1
AgathaP <ebrumaktub@gmx.com>
2019-10-18 07:39:06 -0300

    Noté que el ejercicio de "FizzBuzz" hecho con while tenía un error al momento de imprimir en consola. Subo cambios con esto solucionado. El ejercicio está 100% logrado

--- Casos-practicos/Fizz-Buzz/index.js
+++ Casos-practicos/Fizz-Buzz/index.js
@@ -2,15 +2,17 @@
 // Todo número divisibles por 3 será cambiado por la palabra 'Fizz'
 // Todo número divisibles por 5 será cambiado por la palabra 'Buzz'
 // Para números mdivisbles por ambos será cambiado por la palabra 'FizzBuzz'
-let num = 1;
-while(num <= 100){
-    console.log(num++);
-    if(num % 3 === 0 && num % 5 === 0){
+let num = 0;
+while (num < 100) {
+    num++;
+    if (num % 3 === 0 && num % 5 === 0) {
         console.log('FizzBuzz');
-    } else if (num % 5 === 0){
+    } else if (num % 5 === 0) {
         console.log('Buzz');
-    } else if(num % 3 === 0){
+    } else if (num % 3 === 0) {
         console.log('Fizz');
+    } else {
+        console.log(num);
     }
 }
 
