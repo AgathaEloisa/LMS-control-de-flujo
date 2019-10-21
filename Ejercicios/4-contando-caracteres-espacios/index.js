5d76d1efaf4d82afac79f3bac4f2d2a29f3f37d9
AgathaP <ebrumaktub@gmx.com>
2019-10-21 11:17:34 -0300

    subo avances en el ejercicio, cuento espacios, arrojo resultado en mayúsculas y cantidad de carácteres, sin embargo esa ultima función aún tiene un error

--- Ejercicios/4-contando-caracteres-espacios/index.js
+++ Ejercicios/4-contando-caracteres-espacios/index.js
@@ -1,46 +1,38 @@
 // usar 'do wile' para delimitar la cantidad de palabras que pueden usar.
 // No usar length :(
-    let sentence = prompt('Ingresa aquí la frase que quieras')
+let sentence = prompt('Ingresa aquí la frase que quieras')
 
-    let letter = '';
-    let counter = 0;
-    let spaceCounter = 0;
+let letter = '';
+let counter = 1;
+let spaceCounter = 0;
 
-    // Una función que imprime todos los caracteres de un string, cada uno en una línea separada, en mayúscula.
-    const printStringCharacters = (str) => {
-        for(let i = 0; i < str.length; i++){
-            letter += str[i].toUpperCase() + '\n';
-        }
-        return letter
-    };
-
-    // Una función que cuente el número de caracteres de un string (¡sin utilizar .length!)
-    const characterCounter = (strFunc) =>{
-        for(let i = 0 ;i <= sentence; i++){
-            counter++;
-        }
-            return counter;
+// 1- Una función que imprime todos los caracteres de un string, cada uno en una línea separada, en mayúscula.
+const printStringCharacters = (sentence) => {
+    for (let i = 0; i < sentence.length; i++) {
+        letter += sentence[i].toUpperCase() + '\n';
     }
+    return letter
+};
 
-    console.log(printStringCharacters(sentence));
-    console.log(characterCounter(printStringCharacters(sentence)));
+// Una función que cuente el número de caracteres de un string (¡sin utilizar .length!)
+const characterCounter = (sentence) => {
+    let last = sentence.substr(-1);
+    //cuenta los carácteres pero sólo hasta que se topa con uno coincida con la ultima letra
+    // let first = sentence.substr(0,1);
+    // let total = sentence.lastIndexOf
+    return counter += sentence.indexOf(last, 0);
+}
 
-    // let spaceCounter = 0;
-    // let counter = 0;
-    // let letter = '';
-    // for(let i = 0; i < sentence.length; i++){
-    //     //    Muestra en consola los caracteres uno a uno, separados por un salto de linea.
-    //     letter += sentence[i].toUpperCase() + '\n';
-    //    if(sentence[i] === ' '){
-    //     //    Cuenta espacios.
-    //         spaceCounter++;
-    //    } else if (sentence[i] !== ' '){
-    //     //    cuenta caracteres que no son espacios.
-    //         counter++;
-    //    }
-    // }
-    
-    // console.log(letter);
-    // console.log(counter);
-    // console.log(spaceCounter);
-    // Una función que cuente el número de espacios de un string
+// Una función que cuente el número de espacios de un string
+const counterSpaces = (sentence) => {
+    for (let i = 0; i < sentence.length; i++){
+        if(sentence[i] === ' '){
+            spaceCounter++;
+        }
+    }
+    return spaceCounter;
+}
+
+console.log(printStringCharacters(sentence));
+console.log('leter number', characterCounter(sentence));
+console.log('spaces in the text', counterSpaces(sentence));
